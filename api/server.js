3af660e283eeaa98623ee956a872e688cc02bc62
Samuel Conde <samuelconra@gmail.com>
2025-05-13 17:16:30 -0600

    Main Endpoints Created

--- api/server.js
+++ api/server.js
@@ -9,34 +9,39 @@ const PORT = 3000;
 
 app.use(cors());
 
-app.get('/locations', async (req, res) => {
-//   const result = await pool.query(`
-//     SELECT id, nombre, ST_AsGeoJSON(ubicacion) AS geom FROM ubicaciones;
-//   `);
-//   const geojson = {
-//     type: "FeatureCollection",
-//     features: result.rows.map(row => ({
-//       type: "Feature",
-//       geometry: JSON.parse(row.geom),
-//       properties: { id: row.id, nombre: row.nombre }
-//     }))
-//   };
-//   res.json(geojson);
+// GET HOTELS
+app.get('/hotels', async (req, res) => {
+    const result = await pool.query(`
+            SELECT id, nombre, estrellas, direccion, precio, mascotas, estacionamiento, 
+            ST_AsGeoJSON(coordenadas) AS coord FROM Hoteles;
+    `);
+
+    const geojson = {
+        type: "FeatureCollection",
+        features: result.rows.map(row => ({
+            type: "Feature",
+            geometry: JSON.parse(row.geom),
+            properties: { id: row.id, nombre: row.nombre }
+        }))
+    };
+    res.json(geojson);
 });
 
-app.get('/zones', async (req, res) => {
-//   const result = await pool.query(`
-//     SELECT id, nombre, ST_AsGeoJSON(ubicacion) AS geom FROM zonas;
-//   `);
-//   const geojson = {  
-//     type: "FeatureCollection",
-//     features: result.rows.map(row => ({
-//       type: "Feature",
-//       geometry: JSON.parse(row.geom), properties: { id: row.id, nombre: row.nombre }
-//     }))
-//   };
-//   res.json(geojson);
+// GET PARKS
+app.get('/parks', async (req, res) => {
+    const result = await pool.query(`
+        SELECT id, nombre, accesible, ST_AsGeoJSON(coordenadas) AS coord FROM Parques;
+    `);
+
+    const geojson = {  
+        type: "FeatureCollection",
+        features: result.rows.map(row => ({
+        type: "Feature",
+        geometry: JSON.parse(row.geom), 
+        properties: { id: row.id, nombre: row.nombre }
+        }))
+    };
+    res.json(geojson);
 });
   
-
-app.listen(PORT, () => console.log(`Servidor en http://${HOST}:${PORT}`));
+app.listen(PORT, () => console.log(`API en http://${HOST}:${PORT}`));
